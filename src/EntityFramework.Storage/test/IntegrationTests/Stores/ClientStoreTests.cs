b266baf7c9fd951246d3bccaba3937654db6025c
Johan Majoor <jmajoor@sparklinglogic.com>
2024-04-09 12:42:51 -0700

    Update xunit, fluentassertions, microsoft test sdk

--- src/EntityFramework.Storage/test/IntegrationTests/Stores/ClientStoreTests.cs
+++ src/EntityFramework.Storage/test/IntegrationTests/Stores/ClientStoreTests.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
@@ -146,9 +146,11 @@ namespace IdentityServer4.EntityFramework.IntegrationTests.Stores
                 const int timeout = 5000;
                 var task = Task.Run(() => store.FindClientByIdAsync(testClient.ClientId));
 
-                if (await Task.WhenAny(task, Task.Delay(timeout)) == task)
+                var result = await Task.WhenAny(task, Task.Delay(timeout));
+
+                if (result == task)
                 {
-                    var client = task.Result;
+                    var client = await task;
                     client.Should().BeEquivalentTo(testClient);
                 }
                 else
